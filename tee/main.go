35aac6cd7088692dc54c811a0685660912766c57
Tuan Vuong <vanhtuan0409@gmail.com>
2019-11-07 18:38:35 +0700

    implement append flag

--- tee/main.go
+++ tee/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"os"
 )
 
+var (
+	append = flag.Bool("a", false, "Append to the end of file")
+)
+
 func main() {
-	if len(os.Args) != 2 {
-		panic("Required output file")
+	flag.Parse()
+	fFlag := os.O_APPEND | os.O_CREATE | os.O_WRONLY
+	if !*append {
+		fFlag |= os.O_TRUNC
 	}
 
-	f, err := os.OpenFile(os.Args[1], os.O_APPEND|os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	args := flag.Args()
+	if len(args) != 1 {
+		panic("Required output file")
+	}
+	f, err := os.OpenFile(args[0], fFlag, 0644)
 	if err != nil {
 		panic(err)
 	}
